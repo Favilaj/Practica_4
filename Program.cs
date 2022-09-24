bd95fba77278469128a687451bdadd1ef6f1aa5e
WIN10PRO <WIN10PRO@DESKTOP-34KC35S>
2022-09-23 20:46:45 -0700

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa la temperatura en Celsius: ");
+
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            double f = (c * 9 / 5) + (32);
+
+            Console.WriteLine(c + "ºC es equivalente ha " + f + "ºF");
+
+            Console.ReadLine();
+        }
+    }
+}
